f97dfc7c7edd6d645320cece894f62d0dba7e947
fathmamehnoor <fathmamehnoor@gmail.com>
2024-03-03 00:01:23 +0530

    Added new algorithms

--- encryptdecrypt.js
+++ encryptdecrypt.js
@@ -1,5 +1,5 @@
-// Encryption function
-function encrypt() {
+//AES Encryption function
+function aes_encrypt() {
   const key = document.querySelector("#key").value;
   const message = document.querySelector("#message").value;
 
@@ -9,13 +9,57 @@ function encrypt() {
   document.querySelector("#result").value = encryptedMessage;
 }
 
-// Decryption function
-function decrypt() {
+//AES Decryption function
+function aes_decrypt() {
   const key = document.querySelector("#key").value;
   const encryptedMessage = document.querySelector("#message").value;
 
   // Decrypt the encrypted message using the provided key
   const decryptedMessage = CryptoJS.AES.decrypt(encryptedMessage, key).toString(CryptoJS.enc.Utf8);
+
+  document.querySelector("#result").value = decryptedMessage;
+}
+
+//Triple DES Encryption function
+function triple_des_encrypt() {
+  const key = document.querySelector("#key").value;
+  const message = document.querySelector("#message").value;
+
+  // Encrypt the message using Triple DES encryption with the provided key
+  const encryptedMessage = CryptoJS.TripleDES.encrypt(message, key).toString();
+
+  document.querySelector("#result").value = encryptedMessage;
+}
+
+//Triple DES Decryption function
+function triple_des_decrypt() {
+  const key = document.querySelector("#key").value;
+  const encryptedMessage = document.querySelector("#message").value;
+
+  // Decrypt the encrypted message using the provided key
+  const decryptedMessage = CryptoJS.TripleDES.decrypt(encryptedMessage, key).toString(CryptoJS.enc.Utf8);
+
+  document.querySelector("#result").value = decryptedMessage;
+}
+
+//RC4 Encryption function
+function rc4_encrypt() {
+  const key = document.querySelector("#key").value;
+  const message = document.querySelector("#message").value;
+
+  // Encrypt the message using RC4 encryption with the provided key
+  const encryptedMessage = CryptoJS.RC4.encrypt(message, key).toString();
+
+  document.querySelector("#result").value = encryptedMessage;
+}
+
+//RC4 Decryption function
+function rc4_decrypt() {
+  const key = document.querySelector("#key").value;
+  const encryptedMessage = document.querySelector("#message").value;
+
+  // Decrypt the encrypted message using the provided key
+  const decryptedMessage = CryptoJS.RC4.decrypt(encryptedMessage, key).toString(CryptoJS.enc.Utf8);
 
   document.querySelector("#result").value = decryptedMessage;
 }
